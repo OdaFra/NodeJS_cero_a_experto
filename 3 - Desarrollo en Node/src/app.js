08c0071f35f7d52844455b483a06e6feaa0cbb8d
Oscar Ramirez <osramirezf@gmail.com>
2024-01-08 21:04:12 -0300

    feat: Refactor callback -> arrow function

--- 3 - Desarrollo en Node/src/app.js
+++ 3 - Desarrollo en Node/src/app.js
@@ -1,3 +1,14 @@
-const { emailTemplate } = require('./js-fundation/01-template')
-require('./js-fundation/02-destructuring');
+// const { emailTemplate } = require('./js-fundation/01-template')
+// require('./js-fundation/02-destructuring');
 // console.log(emailTemplate)
+// const { getUserById } =require('./js-fundation/03-callback');
+const { getUserById } =require('./js-fundation/04-arrows.js');
+
+const id = 2
+
+getUserById(id, (error, user)=>{
+    if (error){
+        throw new Error(error)
+    }
+    console.log({user})
+})
