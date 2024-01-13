e24d0b590e260cd966dcf3646420d2348f1ebe1d
Oscar Ramirez <osramirezf@gmail.com>
2024-01-13 18:07:07 -0300

    feat: Implementando ejemplo de arquitectura limpia para manejo de dependencias

--- 3-Desarrollo en Node/src/js-fundation/05-factory.js
+++ 3-Desarrollo en Node/src/js-fundation/05-factory.js
@@ -1,12 +1,13 @@
-const { v4: uuidv4 } = require('uuid')
-const getAge = require('get-age')
+// const { v4: uuidv4 } = require('uuid')
+// const getAge = require('get-age')
+const { getAge, getUuid } = require('../plugins')
 
 const obj = {name:'Oscar', birthdate:'1991-01-09'}
 
 const buildPerson = ({name, birthdate})=>{
 
     return{
-        id: uuidv4(),
+        id: getUuid(),
         name:name,
         birthdate:birthdate,
         age: getAge(birthdate)
