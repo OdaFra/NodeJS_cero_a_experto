380ee0abe3ce15e4e72f5fa9599612eaabac9413
Oscar Ramirez <osramirezf@gmail.com>
2024-01-13 18:28:02 -0300

    feat: Factory aplicado

--- 3-Desarrollo en Node/src/app.js
+++ 3-Desarrollo en Node/src/app.js
@@ -4,7 +4,15 @@
 // const { getUserById } =require('./js-fundation/03-callback');
 // const { getUserById } =require('./js-fundation/04-arrows.js');
 
-require('./js-fundation/05-factory')
+ const { getAge, getUuid } = require("./plugins");
+const { buildMakePerson } = require('./js-fundation/05-factory')
+
+const makePerson = buildMakePerson({ getUuid, getAge })
+
+const obj = ({ name: 'Oscar', birthdate:'1991-01-09' })
+
+const oscar = makePerson(obj)
+console.log({ oscar })
 
 // const id = 2
 
--- 3-Desarrollo en Node/src/js-fundation/05-factory.js
+++ 3-Desarrollo en Node/src/js-fundation/05-factory.js
@@ -1,18 +1,19 @@
 // const { v4: uuidv4 } = require('uuid')
 // const getAge = require('get-age')
-const { getAge, getUuid } = require('../plugins')
+// const { getAge, getUuid } = require("../plugins");
+// const obj = { name: "Oscar", birthdate: "1991-01-09" };
 
-const obj = {name:'Oscar', birthdate:'1991-01-09'}
+const buildMakePerson = ({ getUuid, getAge }) => {
+  return ({ name, birthdate }) => {
+    return {
+      id: getUuid(),
+      name: name,
+      birthdate: birthdate,
+      age: getAge(birthdate),
+    };
+  };
+};
 
-const buildPerson = ({name, birthdate})=>{
-
-    return{
-        id: getUuid(),
-        name:name,
-        birthdate:birthdate,
-        age: getAge(birthdate)
-    }
-}
-const oscar = buildPerson(obj)
-
-console.log(oscar)
+module.exports = {
+  buildMakePerson,
+};
