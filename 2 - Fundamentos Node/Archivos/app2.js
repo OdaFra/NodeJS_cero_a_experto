c2dbbe261585696ae31352be184baea616a5b586
Oscar Ramirez <osramirezf@gmail.com>
2024-01-13 16:58:35 -0300

    fix: Reenombramos carpetas para mejor manejo!

--- 2 - Fundamentos Node/Archivos/app2.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const data =   fs.readFileSync('readme.md', 'utf8')
-
-console.log(data)
-
-const newData = data.replace(/React/ig, 'Angular')
-
-fs.writeFileSync('Readme-Angular.md', newData);
--- 3 - Desarrollo en Node/src/js-fundation/02-destructuring.js
+++
@@ -1,9 +0,0 @@
-// console.log(process.env.PORT ?? 3300);
-
-const { SHELL } = process.env
-// console.log(SHELL)
-
-const characters = ['Ironman', 'Thor', 'Cap', 'Black Panter']
-
-// const [ ,Thor ] =  characters;
-// console.log(Thor)
