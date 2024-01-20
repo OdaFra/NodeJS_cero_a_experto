b585c86fa5ef7136853565383a1b943a50232f31
Oscar Ramirez <osramirezf@gmail.com>
2024-01-20 17:10:37 -0300

    feat: Implementando httpCliente para estructura limpia.

---
+++ 3-Desarrollo en Node/src/plugins/http-clients.plugin.js
@@ -0,0 +1,12 @@
+const httpClientPlugin = {
+  get: async (url) => {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+  },
+  post: async (url, body) => {},
+  put: async (url, body) => {},
+  delete: async (url, body) => {},
+};
+
+module.exports = { httpClientPlugin };
