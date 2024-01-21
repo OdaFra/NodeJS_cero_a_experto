9eaf35498e6c9e52f413b6311b44f9caa2236a26
Oscar Ramirez <osramirezf@gmail.com>
2024-01-21 11:34:52 -0300

    Agregamos httpCliente a arribo de barril.

--- 3-Desarrollo en Node/src/plugins/index.js
+++ 3-Desarrollo en Node/src/plugins/index.js
@@ -1,7 +1,9 @@
 const { getAge } = require("../plugins/get-age.plugin");
 const { getUuid } = require("../plugins/uuid.plugin");
+const { http } = require("../plugins/http-clients.plugin");
 
 module.exports = {
   getAge,
   getUuid,
+  http,
 };
