c0d9684956eaa2b53414d9abe6c9ef97fd1a953f
Oscar Ramirez <osramirezf@gmail.com>
2024-02-03 17:08:20 -0300

    barril: Agregamos import buildLogger para import logger.plugins.

--- 3-Desarrollo en Node/src/plugins/index.js
+++ 3-Desarrollo en Node/src/plugins/index.js
@@ -1,9 +1,11 @@
 const { getAge } = require("../plugins/get-age.plugin");
 const { getUuid } = require("../plugins/uuid.plugin");
 const { http } = require("../plugins/http-clients.plugin");
+const buildLogger = require('../plugins/logger.plugins')
 
 module.exports = {
   getAge,
   getUuid,
   http,
+  buildLogger
 };
