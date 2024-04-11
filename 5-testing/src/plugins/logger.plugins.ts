c1a17cad621ca82b86935f51a65dc5fc702bf241
Oscar Ramirez <osramirezf@gmail.com>
2024-04-11 17:04:39 -0400

    test: Aplicamos test para el archivo logger.plugins

--- 5-testing/src/plugins/logger.plugins.ts
+++ 5-testing/src/plugins/logger.plugins.ts
@@ -4,18 +4,24 @@ import winston, { format} from 'winston'
 // const winston = require('winston');
 const { combine, timestamp, json } = format;
 
-const logger = winston.createLogger({
+export const logger = winston.createLogger({
     level:'info',
     format: //winston.format.json(),
     combine(
         timestamp(), json()
     ),
-    defaultMeta: {service: 'user-service'},
+    // defaultMeta: {service: 'user-service'},
     transports:[
         new winston.transports.File({ filename:'error.log', level:'error' }),
         new winston.transports.File({ filename:'combined.log' })
     ]
 })
+
+logger.add(
+    new winston.transports.Console({
+        format: winston.format.simple()
+    })
+)
 
 export const  buildLogger= (service:string)=>{
     return {
